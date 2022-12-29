512cd0fc1df56c79f05bf6eb5f537779010d271c
jahidhasan8 <jahidhasan1932@gmail.com>
2022-12-29 17:05:41 +0600

    api created

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(express.json())
 
 
 
-
+// DB_USER=alpha
+// DB_PASSWORD=cKFZz9lbFIFHGZ9Z
 
 
 
@@ -24,6 +25,33 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 
 
+async function run(){
+  try{
+    
+    const gamesCollection = client.db('alpha').collection('games')
+
+    app.post('/games',async(req,res)=>{
+        const game = req.body
+        const result = await gamesCollection.insertOne(game)
+         res.send(result)
+    })
+
+    app.get('/games', async(req,res)=>{
+        const query={}
+        const result=await gamesCollection.find(query).toArray()
+        res.send(result)
+    })
+
+
+  }
+
+  finally{
+
+  }
+
+}
+
+run().catch(error => console.log(error.message))
 
 
 app.get('/', async (req, res) => {
